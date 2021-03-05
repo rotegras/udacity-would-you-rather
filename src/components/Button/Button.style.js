28bd5c91dbb0e93907c52dc0deeb1985775541c8
Javi Ortega <rotegras@gmail.com>
2021-03-05 16:32:07 +0100

    feat: add shared style for button and link

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -1,19 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 
-const StyledLink = styled(Link)`
-  border: 1px solid #eee;
-  font-size: {({theme}) => theme.fontSize.small};
-  padding: .5rem;
+const buttonCss = css`
+  align-items: center;
+  background-color: ${({theme}) => theme.button.background};
+  border: none;
+  border-radius: ${({theme}) => theme.borderRadius};
+  box-sizing: border-box;
+  color: ${({theme}) => theme.button.color};
+  display: flex;
+  font-size: ${({theme}) => theme.fontSize.small};
+  font-weight: 900;
+  height: 20px;
+  padding: .75rem 1rem;
   text-decoration: none;
+  text-transform: uppercase;
+
+  &:disabled {
+    background-color: ${({theme}) => theme.button.disabled.background};
+    color: ${({theme}) => theme.button.disabled.color};
+  }
+  &:not(:disabled):hover {
+    background-color: ${({theme}) => theme.button.hover.background};
+    color: ${({theme}) => theme.button.hover.color};
+  }
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonCss};
 `;
 
 const StyledButton = styled.button`
-  border: 1px solid #eee;
-  font-size: {({theme}) => theme.fontSize.small};
-  padding: .5rem;
-  text-decoration: 'none;
+  ${buttonCss};
 `;
 
 
