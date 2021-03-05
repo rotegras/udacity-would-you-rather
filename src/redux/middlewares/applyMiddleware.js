0c779dbbf607ab18597d2bcb20b83d35bd40bdf4
Javi Ortega <rotegras@gmail.com>
2021-03-05 17:33:25 +0100

    feat: add persistant state with sessionstorage

--- src/redux/middlewares/applyMiddleware.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware } from 'redux';
-import logger from './logger';
-import thunk from 'redux-thunk';
-
-
-export default applyMiddleware(
-  thunk,
-  logger,
-);
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,17 +1,45 @@
 import { createStore, compose } from 'redux';
-import middleware from '../middlewares';
+// import middleware from '../middlewares';
 import rootReducer from '../reducers';
+import { applyMiddleware } from 'redux';
+import logger from '../middlewares/logger';
+import thunk from 'redux-thunk';
 
 
+const ConfigureStore = () => {
+
+  let initialState = {};
+
+  try {
+    initialState = sessionStorage.getItem('wouldYouRatherState')
+      ? JSON.parse(sessionStorage.getItem('wouldYouRatherState'))
+      : {};
+  } catch (error) {
+    console.log('getError', error)
+  }
+
+  const saveToLocalStorage = (store) => (next) => (action) => {
+    const stateToSave = store.getState();
+    sessionStorage.setItem('wouldYouRatherState', JSON.stringify({
+      ...stateToSave
+    }));
+    return next(action);
+  }
 
   const store = createStore(
     rootReducer,
+    initialState,
     compose(
-      middleware,
+      applyMiddleware(
+        thunk,
+        saveToLocalStorage,
+        logger,
+      ),
       // eslint-disable-next-line no-underscore-dangle
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     )
   );
+  return store;
+}
 
-export default store;
-// export { history };
+export default ConfigureStore;
