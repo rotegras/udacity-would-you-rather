0b693cea5992ee7fbcc43b3a461b7728a4902143
Javi Ortega <rotegras@gmail.com>
2021-03-14 20:14:22 +0100

    refactor: comment redux devtools

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -36,7 +36,7 @@ const ConfigureStore = () => {
         logger,
       ),
       // eslint-disable-next-line no-underscore-dangle
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     )
   );
   return store;
